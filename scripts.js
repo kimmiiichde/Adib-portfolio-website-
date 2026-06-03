0787052224137a4465a7862244e171ef3c3c9e99
kimmiiiichde <adibhasanovi2006@gmail.com>
2026-06-03 09:29:32 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+console.log("Portfolio Website Loaded");
+
+document.querySelector("form").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Thank you! Your message has been received.");
+});
